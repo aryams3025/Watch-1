a11669ef5a3a91bc3309c1f73f008d4f8e2a9a8d
aryams3025 <80573229+aryams3025@users.noreply.github.com>
2024-03-11 12:00:40 +0530

    modified

--- model/productModel.js
+++ model/productModel.js
@@ -29,6 +29,10 @@ const productSchema = Schema({
         type: Number,
         required: true
     },
+    discount: {
+        type: Number,
+        required: true
+    },
     image: {
         type: Array,
         required: true
