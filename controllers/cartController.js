1455e0c7626fa172ef2a56b97749a6c969ebbc95
aryams3025 <80573229+aryams3025@users.noreply.github.com>
2024-03-13 13:25:40 +0530

    changed

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,3 +1,51 @@
-const cartSchema = require('../model/cartModel')
-const cartHelper = require('../helpers/cartHelper')
-const productSchema = require('../model/productModel')
+const cartSchema = require('../model/cartModel');
+const cartHelper = require('../helpers/cartHelper');
+const productSchema = require('../model/productModel');
+
+getCart : async (req,res)=>{
+    try {
+        // Extracting user session
+        const { user } = req.session;
+
+        // Variable to store the count of the products after updating quantity 
+        let productCount;
+
+        // Updating the quantities in the cart and documenting product count in session if necessary
+        productCount = await cartHelper.updateQuantity(user);
+
+        if (productCount) {
+            req.session.productCount--;
+        }
+
+        // Then retrieving updated cart with populated product information
+        const updatedCart = await cartSchema.findOne({ userId: user })
+            .populate({
+                path: 'items.productId',
+                populate: {
+                    path: 'category'
+                }
+            });
+
+        
+        console.log(updatedCart);
+
+        const totalPrice = await cartHelper.totalCartPrice(user)
+
+        // checking if cart exists and has time 
+        if(updatedCart && updatedCart.items > 0 ){
+            // updating prices
+            updatedCart.items.forEach(items => {
+                return items
+            });
+        }
+
+        res.render('shop/cart',{
+            cartItems : updatedCart,
+            totalAmount : totalPrice
+        })
+
+    } catch (error) {
+        console.log(error);
+        
+    }
+};
--- routers/shopRouter.js
+++ routers/shopRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const shopController = require('../controllers/shopController');
 const isAuth = require('../middleware/isAuth');
-const cartSchema = require('../controllers/cartController')
+const cartController = require('../controllers/cartController')
 // router.get('/', shopController.getHome);
 router.get('/shop',shopController.getShop);
 router.get('/products/:id',shopController.getSingleProduct)
